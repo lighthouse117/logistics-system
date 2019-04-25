81f10580694cd1264eb45c0bb732cd7591386cae
lighthouse117 <>
2019-04-25 11:53:40 +0900

    change href to url, add base templates and change width of table

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
- 
+
 urlpatterns = [
     path('', views.select_base, name='select_base'),
+    path('<slug:base_name>/home/', views.home, name='home'),
+    path('<slug:base_name>/stock/', views.stock, name='stock'),
+    path('<slug:base_name>/staff/', views.staff, name='staff'),
+    path('<slug:base_name>/distribution/', views.distribution, name='distribution'),
 ]
--- logistics/views.py
+++ logistics/views.py
@@ -2,6 +2,36 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from .models import Stock
 
+
 def select_base(request):
+    return render(request, 'logistics/select_base.html')
+
+
+def home(request, base_name):
+    context = {
+        'base_name': base_name,
+    }
+    return render(request, 'logistics/home.html', context)
+
+
+def stock(request, base_name):
     stocks = Stock.objects.all()
-    return render(request, 'logistics/stock.html', {'stocks': stocks})
+    context = {
+        'base_name': base_name,
+        'stocks': stocks,
+    }
+    return render(request, 'logistics/stock.html', context)
+
+
+def staff(request, base_name):
+    context = {
+        'base_name': base_name,
+    }
+    return render(request, 'logistics/staff.html', context)
+
+
+def distribution(request, base_name):
+    context = {
+        'base_name': base_name,
+    }
+    return render(request, 'logistics/distribution.html', context)
