e35a1332d929e9b65d025b97fd629742b7ca4389
lighthouse117 <>
2019-08-18 10:28:13 +0900

    add input_stock.html

--- logistics/urls.py
+++ logistics/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.select_base, name='select_base'),
+    path('select_base/', views.select_base, name='select_base'),
     path('<slug:base_name>/home/', views.home, name='home'),
     path('<slug:base_name>/stock/', views.stock, name='stock'),
+    path('<slug:base_name>/stock/input_stock', views.input_stock, name='input_stock'),    
     path('<slug:base_name>/staff/', views.staff, name='staff'),
     path('<slug:base_name>/distribution/', views.distribution, name='distribution'),
 ]
--- logistics/views.py
+++ logistics/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.shortcuts import redirect
 from .models import Stock
+from .forms import StockForm
 
 
 def select_base(request):
@@ -21,6 +23,26 @@ def stock(request, base_name):
         'stocks': stocks,
     }
     return render(request, 'logistics/stock.html', context)
+
+
+def input_stock(request, base_name):
+    if request.method == "POST":
+        form = StockForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            stocks = Stock.objects.all()
+            context = {
+                'base_name': base_name,
+                'stocks': stocks,
+            }
+        return redirect('stock', kwargs=context)
+    else:
+        form = StockForm()
+        context = {
+            'base_name': base_name,
+            'form': form,
+        }
+    return render(request, 'logistics/input_stock.html', context)
 
 
 def staff(request, base_name):
